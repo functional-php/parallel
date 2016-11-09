ccc8f08bd20ce8b249955986fd8e0d25f9a9cf1f
Gilles Crettenand <gilles.crettenand@liip.ch>
2016-11-09 13:58:06 +0100

    disable code coverage as it seems it don't work when only functions are tested

--- .atoum.php
+++ .atoum.php
@@ -7,9 +7,11 @@ $report = $script->addDefaultReport();
 $coverageField = new atoum\report\fields\runner\coverage\html('Pattern Matching', './reports/');
 $report->addField($coverageField);
 
+/*
 $cloverWriter = new atoum\writers\file('./reports/atoum.coverage.xml');
 $cloverReport = new atoum\reports\asynchronous\clover();
 $cloverReport->addWriter($cloverWriter);
 $runner->addReport($cloverReport);
+*/
 
 $runner->addTestsFromDirectory('./tests');
